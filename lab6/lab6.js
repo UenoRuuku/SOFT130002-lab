853c6a63eed98953c6d429cabb7f3ad2b5b2b34e
Ruuku <18302010013@fudan.edu.cn>
2019-05-02 21:54:24 +0800

    imit

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -1,29 +1,33 @@
 window.onload = function(){
     var timerIn = null;
     var timerOut = null;
-    var figcaption = document.getElementsByTagName("figcaption")[0];
     var imgs = document.getElementsByTagName("img");
     var $bImage = document.getElementById("featured");
 
     for(var i = 0 ; i<imgs.length ; i++) {
     imgs[i].index = i;
     imgs[i].onclick = function () {
-        $bImage.innerHTML = "<img " + "src=" + "\"" + this.src.replace(/small/g, "medium") + "\" title = \"" + this.title + "\"/>"
+        $bImage.innerHTML = "<img " + "src=" + "\"" + this.src.replace(/small/g, "medium") + "\" title = \"" + this.title + "\"/>"+
         "<figcaption>" + this.title + "</figcaption>";
+        // console.log($bImage.innerHTML)
+
     }
 }
-$bImage.onclick = function () {
-    alert("fuck")
-}
+// $bImage.onclick = function () {
+//     alert("fuck")
+// }
+
+// 注意要重新引用
 $bImage.onmouseleave = function () {
+    var figcaption = document.getElementsByTagName("figcaption")[0];
     clearInterval(timerIn)
     var opacity = figcaption.style.opacity;
     timerOut = setInterval(function () {
         var doubleOp = figcaption.style.opacity;
         doubleOp -= 0.02;
         figcaption.style.opacity = String(doubleOp);
-        console.log("op="+doubleOp);
-        console.log("sb="+figcaption.style.opacity)
+        // console.log("op="+doubleOp);
+        // console.log("sb="+figcaption.style.opacity)
         if (doubleOp === 0) {
             clearInterval(timerOut);
         }
@@ -31,6 +35,7 @@ $bImage.onmouseleave = function () {
 }
 
 $bImage.onmouseenter = function () {
+    var figcaption = document.getElementsByTagName("figcaption")[0];
     clearInterval(timerOut)
     timerIn = setInterval(function () {
         var doubleOp = figcaption.style.opacity*100;
